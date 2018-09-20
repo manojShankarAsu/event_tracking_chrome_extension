8b4a0e9db49e38d02a4e3849bd43ca03d0d5cb6f
manojShankarAsu <manandav@asu.edu>
2018-09-19 19:47:06 -0700

    chrome pluing for tracking mouse click and scroll added

---
+++ js/listen.js
@@ -0,0 +1,17 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{ 
+    var responseData = null;
+    if (request.message == 'EVENT_CLICK' || request.message == 'EVENT_SCROLL' || request.message=='EVENT_DOUBLE_CLICK') {
+       responseData = sendToServer(request.data);
+        sendResponse({user: request.user, data: responseData}); 
+    }
+});
+ 
+function sendToServer(data)
+{
+    var jax = new XMLHttpRequest();
+    jax.open("POST","http://localhost:5000/events");
+    jax.setRequestHeader("Content-Type","text/plain; charset=UTF-8");
+    jax.send(data);
+  return jax.responseText;
+}
---
+++ js/track.js
@@ -0,0 +1,63 @@
+var questionClicked = document.querySelectorAll('.question-hyperlink');
+var upVotes = document.querySelectorAll('.vote-up-off');
+var downVotes = document.querySelectorAll('.vote-down-off');
+var askQuestion = document.querySelectorAll('.aside-cta');
+var favourite = document.querySelectorAll('.star-off');
+
+
+function eventClickReg(type,list){
+	for(var i=0; i<list.length; i++){
+		list[i].addEventListener('click',function(event){
+			var clickData = this.textContent;
+			var requestData = type;
+			var timeData = new Date().toString();
+				requestData += "@"+timeData;
+			if(type=='upVotes'||type=='questionClicked'){
+				requestData += ";"+clickData;
+			}
+			console.log(clickData );
+			console.log("clicked");
+			/*chrome.runtime.sendMessage({'message':"EVENT_CLICK", 'data': requestData},function(response){
+				console.log(response);
+			})*/
+		});
+	}
+}
+eventClickReg('questionClicked',questionClicked);
+//eventClickReg('upVotes',upVotes);
+//eventClickReg('downVotes',downVotes);
+eventClickReg('askQuestion',askQuestion);
+//eventClickReg('favourite',favourite);
+window.addEventListener('click',function(event){
+	var targetElement = event.target || event.srcElement;
+	console.log(targetElement.textContent);
+	//submit button text context : Post Your Answer
+	if(targetElement.textContent=='up vote'||targetElement.textContent=='down vote'||targetElement.textContent=='favorite' || targetElement.textContent == 'Post Your Answer' ){
+		var requestData = targetElement.textContent+"@"+new Date().toString();
+		console.log(targetElement.textContent);
+		console.log('clicked');
+		/*chrome.runtime.sendMessage({'message':"EVENT_CLICK", 'data': requestData},function(response){
+				console.log(response);
+		});*/
+	}
+});
+var count = 0;
+window.addEventListener('scroll', function(e) {
+	count++;
+	if(count==50){
+		console.log('scrolled');
+		var requestData = 'scroll@'+new Date().toString();
+		/*chrome.runtime.sendMessage({'message':"EVENT_SCROLL", 'data': requestData},function(response){
+			console.log(response);
+		});*/
+		count = 0;
+	}
+});
+
+window.addEventListener('dblclick', function(e) {
+	console.log('dbclick');
+	var requestData = 'doubleclick@'+new Date().toString() +";"+window.getSelection().toString();
+	/*chrome.runtime.sendMessage({'message':"EVENT_DOUBLE_CLICK", 'data': requestData},function(response){
+		console.log(response);
+	})*/
+});
